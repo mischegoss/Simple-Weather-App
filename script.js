c56f016f437db6ecb6882be61b5a455c8ae72b77
Tamar Auber <tamarauber@Tamars-MBP.cable.rcn.com>
2019-11-06 14:29:29 -0500

    Add image API

--- script.js
+++ script.js
@@ -33,17 +33,22 @@ function findLocation () {
 
 function cardData() {
     const appId = "da9ee3ddb46b386ecd69f797bbede23b";
-    const cityName = currentLocation;
+    const cityName = (currentLocation).replace(/\s+/g, '');
     console.log(cityName)
     const weather = "http://api.openweathermap.org/data/2.5/weather?q=" + currentLocation + "&APPID=" + appId;
 
     $.getJSON(weather,function(json){
         console.log(json)
         temperature = Math.round((parseInt(json.main.temp) * 9/5) - 459.67);
-        forecast = json.weather[0].description;
+        forecast = (json.weather[0].description)
         humidity = json.main.humidity;
 
-       console.log(temperature, forecast, humidity)
+       console.log(temperature, forecast, humidity);
+       let forecastAsString = forecast.replace(/\s+/g, '');
+       let myImage = "https://source.unsplash.com/200x200/?"+ "wheeling" + ",city,weather," + forecastAsString;
+       console.log(myImage)
+       $(".card-img-top").attr('src', myImage);
+$(".temperature").text(temperature);
     });
     
 
@@ -63,6 +68,7 @@ class WeatherDisplay {
 
 //This creates the card
 makeCard()  {
+let myImage = "https://source.unsplash.com/300x200/?" + this.forecast + "," + this.name;
 let newCard = $(".sample-card");
 let myCard= $("<div/>")
 myCard.addClass("card");
@@ -72,15 +78,15 @@ myCard.html("<img class='card-img-top' src='#' alt='Card image cap'>")
 let cardBody = $("<div/>");
 console.log(cardBody)
 cardBody.addClass("card-body");
-cardBody.html("<li class='list-group-item'>one</li> <li class='list-group-item'>two</li>  <li class='list-group-item'>three</li> ");
+cardBody.html("<li class='list-group-item temperature'>one</li> <li class='list-group-item forecast'>two</li>  <li class='list-group-item humidity'>three</li> ");
 myCard.append(cardBody);
 
-$(".sample-card").append(myCard);
+newCard.append(myCard);
 
 
 }}
 
-let s = new WeatherDisplay(currentLocation);
+let s = new WeatherDisplay(currentLocation, temperature, forecast, humidity  );
 
 findLocation()
 
